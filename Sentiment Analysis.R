3473fffdba10a9055409ea4f330a5affe0223c06
Adi Nurrachman <48783354+adinur21@users.noreply.github.com>
2021-02-10 14:16:52 +0700

    Add files via upload

---
+++ Sentiment Analysis.R
@@ -0,0 +1,147 @@
+# Author: Adi Nurrachman
+# Date  : 15 November 2020
+# Dosen : Prof. Suhartono
+
+# Install
+install.packages("tm")  # for text mining
+install.packages("SnowballC") # for text stemming
+install.packages("wordcloud") # word-cloud generator 
+install.packages("RColorBrewer") # color palettes
+install.packages("syuzhet") # for sentiment analysis
+install.packages("ggplot2") # for plotting graphs
+install.packages('rsconnect') #for connect to shiny.io
+
+# Load
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+library("syuzhet")
+library("ggplot2")
+library(rsconnect)
+
+# Read the text file from local machine , choose file interactively
+#text <- readLines(file.choose())
+text <- read.delim("30Juz.txt")
+# Load the data as a corpus
+TextDoc <- Corpus(VectorSource(text))
+
+#Replacing "/", "@" and "|" with space
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+TextDoc <- tm_map(TextDoc, toSpace, "/")
+TextDoc <- tm_map(TextDoc, toSpace, "@")
+TextDoc <- tm_map(TextDoc, toSpace, "\\|")
+
+# Convert the text to lower case
+TextDoc <- tm_map(TextDoc, content_transformer(tolower))
+# Remove numbers
+TextDoc <- tm_map(TextDoc, removeNumbers)
+# Remove english common stopwords
+TextDoc <- tm_map(TextDoc, removeWords, stopwords("english"))
+# Remove your own stop word
+# specify your stopwords as a character vector
+TextDoc <- tm_map(TextDoc, removeWords, c("s", "company","team")) 
+# Remove punctuations
+TextDoc <- tm_map(TextDoc, removePunctuation)
+# Eliminate extra white spaces
+TextDoc <- tm_map(TextDoc, stripWhitespace)
+# Text stemming - which reduces words to their root form
+TextDoc <- tm_map(TextDoc, stemDocument)
+
+# Build a term-document matrix
+TextDoc_dtm <- TermDocumentMatrix(TextDoc)
+dtm_m <- as.matrix(TextDoc_dtm)
+# Sort by descearing value of frequency
+dtm_v <- sort(rowSums(dtm_m),decreasing=TRUE)
+dtm_d <- data.frame(word = names(dtm_v),freq=dtm_v)
+# Display the top 10 most frequent words
+head(dtm_d, 10)
+
+# Plot the most frequent words
+barplot(dtm_d[1:10,]$freq, las = 2, names.arg = dtm_d[1:10,]$word,
+        col ="lightgreen", main ="Top 10 most frequent words",
+        ylab = "Word frequencies")
+
+#generate word cloud
+set.seed(1234)
+wordcloud(words = dtm_d$word, freq = dtm_d$freq, min.freq = 5,
+          max.words=100, random.order=FALSE, rot.per=0.40, 
+          colors=brewer.pal(8, "Dark2"))
+
+# Word Association :
+
+# Find associations 
+findAssocs(TextDoc_dtm, terms = c("day","punish","earth"), corlimit = 0.25)
+# Find associations for words that occur at least 50 times
+findAssocs(TextDoc_dtm, terms = findFreqTerms(TextDoc_dtm, lowfreq = 50), corlimit = 0.25)
+# possibly creat a heat map ?
+
+
+# head(d,10) - to see top 10 lines of the get_nrc_sentiment dataframe
+head (d,10)
+
+# regular sentiment score using get_sentiment() function and method of your choice
+# please note that different methods have different scales
+syuzhet_vector <- get_sentiment(text, method="syuzhet")
+
+# see the first 10 elements of the vector
+head(syuzhet_vector,10)
+
+# see median value of vector elements
+# median(syuzhet_vector)
+summary(syuzhet_vector)
+
+# bing
+bing_vector <- get_sentiment(text, method="bing")
+head(bing_vector)
+summary(bing_vector)
+
+#afinn
+afinn_vector <- get_sentiment(text, method="afinn")
+head(afinn_vector)
+summary(afinn_vector)
+
+#nrc
+nrc_vector <- get_sentiment(text, method="nrc")
+head(nrc_vector)
+median(nrc_vector)
+
+#compare the first row of each vector using sign function
+rbind(
+  sign(head(syuzhet_vector)),
+  sign(head(bing_vector)),
+  sign(head(afinn_vector))
+)
+
+# head(d,10) - just to see top 10 lines
+head (d,10)
+
+# run nrc sentiment analysis to return data frame with each row classified as one of the following
+# emotions, rather than a score : 
+# anger, anticipation, disgust, fear, joy, sadness, surprise, trust 
+# and if the sentiment is positive or negative
+d<-get_nrc_sentiment(text)
+
+#transpose
+td<-data.frame(t(d))
+
+#The function rowSums computes column sums across rows for each level of a grouping variable.
+td_new <- data.frame(rowSums(td[2:253]))
+
+#Transformation and cleaning
+names(td_new)[1] <- "count"
+td_new <- cbind("sentiment" = rownames(td_new), td_new)
+rownames(td_new) <- NULL
+td_new2<-td_new[1:8,]
+
+#Plot 1 - count of words associated with each sentiment
+quickplot(sentiment, data=td_new2, weight=count, geom="bar",fill=sentiment,ylab="count")+ggtitle("Survey sentiments Al quran 30 Juz")
+
+#Plot 2 - count of words associated with each sentiment, expressed as a percentage
+#barplot(
+#  sort(colSums(prop.table(d[, 1:8]))), 
+#  horiz = TRUE, 
+#  cex.names = 0.7, 
+#  las = 1, 
+#  main = "Emosididalamtext", xlab="Percentage"
+)
